7beb07943a8daf9f016bbff792fe508b0ce0a18f
pallab-at-tech <pallab861774@gmail.com>
2025-06-11 14:32:05 +0530

    project section done

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import binkeyitDemo from "../assets/binkeyitDemo.png"
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiExpress } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { SiVite } from "react-icons/si";
 
 const Projects = () => {
   return (
@@ -20,17 +25,17 @@ const Projects = () => {
 
               {/* live link and git link */}
               <div className='flex gap-4'>
-                <Link to={"https://binkeyit-full-stack-oddf.vercel.app/"} className='hover:bg-[#9b9a9a4a] p-1 rounded w-fit h-fit'><FaExternalLinkAlt size={15}/></Link>
-                <Link to={"https://github.com/pallab-at-tech/Binkeyit-Full-stack"} className='hover:bg-[#9b9a9a4a] p-1 rounded w-fit h-fit'><FaGithub size={18}/></Link>
+                <Link to={"https://binkeyit-full-stack-oddf.vercel.app/"} className='hover:bg-[#9b9a9a4a] p-1 rounded w-fit h-fit'><FaExternalLinkAlt size={15} /></Link>
+                <Link to={"https://github.com/pallab-at-tech/Binkeyit-Full-stack"} className='hover:bg-[#9b9a9a4a] p-1 rounded w-fit h-fit'><FaGithub size={18} /></Link>
               </div>
             </div>
 
-            <p className='pl-4 pt-1 text-[#d1d3dc] text-sm line-clamp-2'>A full scalable E-commerce website where peaple can shopping</p>
+            <p className='pl-4 pt-1 text-[#cbd5e1] text-sm line-clamp-2'> A fully scalable e-commerce platform with authentication, payments, and admin control.</p>
 
             {/* image and hover to video */}
             <div>
               <div className='w-full p-5 overflow-hidden flex justify-center items-center hover:scale-125 transition-all'>
-                <img src={binkeyitDemo} alt="" className='object-scale-down rounded-md'/>
+                <img src={binkeyitDemo} alt="" className='object-scale-down rounded-md' />
               </div>
               {/* <video src=""></video> */}
             </div>
@@ -39,7 +44,7 @@ const Projects = () => {
             <div className=''>
 
               <div className='pl-3'>
-                <p className='font-semibold  bg-gradient-to-r from-[#efeb05] to-[#f39428] bg-clip-text text-transparent inline-block'>Feature</p>
+                <p className='font-semibold  bg-gradient-to-r from-[#d9ff00] to-[#f39428] bg-clip-text text-transparent inline-block'>Feature</p>
 
                 <ul className='list-disc pl-7 text-[#d1d3dc] text-sm'>
                   <li>User Authentication & Profile</li>
@@ -49,9 +54,39 @@ const Projects = () => {
                 </ul>
               </div>
 
+              {/* tech use */}
+
+               <p className='font-semibold  bg-gradient-to-r from-[#d9ff00] to-[#f39428] bg-clip-text text-transparent inline-block pt-1 pl-3'>Tech</p>
+
+              <div className='flex pl-3 gap-2 pt-2 items-center flex-wrap'>
+
+                <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  react
+                </div>
+                 <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  tailwind
+                </div>
+                 <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  mongodb
+                </div>
+                <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  express
+                </div>
+                <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  node js
+                </div>
+
+                <div className='px-2 py-1 rounded-full text-center text-[13px] bg-[#0b1c2c] text-[#61DBFB] hover:scale-110 transition-all' style={{ textShadow: '0 0 8px #61DBFB' }}>
+                  Vite
+                </div>
+
+              </div>
+
               <div className='flex justify-between pr-10 items-center pl-3 py-3'>
-                <p className='text-sm  bg-gradient-to-r from-[#efeb05] to-[#f39428] bg-clip-text text-transparent'>Will you know more ?</p>
-                <button className='cursor-pointer bg-terniary-dark py-0.5 px-1 rounded-full mb-0.5 hover:scale-110 transition-all bg-gradient-to-r from-[#28a203] to-[#058cb9]'>click here</button>
+                <p className="text-sm bg-gradient-to-r from-[#3af574] to-[#18b4f0] bg-clip-text text-transparent font-medium">
+                  Want to know more?
+                </p>
+                <button className='cursor-pointer bg-terniary-dark py-1 px-1 rounded-full mb-0.5 hover:scale-110 transition-all bg-gradient-to-r from-[#034a1a] to-[#074fbc]'>click here</button>
               </div>
 
             </div>
