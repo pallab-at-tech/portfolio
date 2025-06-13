3f8501d57e87c15bb15af42bc4bce95e72ecbfbf
pallab-at-tech <pallab861774@gmail.com>
2025-06-13 19:52:10 +0530

    till now full responsive

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -7,20 +7,20 @@ import { Link } from 'react-router-dom'
 
 const Contact = () => {
   return (
-    <section className='bg-primary-black text-primary-text extra-font-style pt-10 lg:p-10  pb-5'>
+    <section className='bg-primary-black text-primary-text extra-font-style pt-10 lg:p-10 p-10  pb-5 '>
 
       {/* for margin */}
       <div className="flex justify-center">
         <div className="h-[1px] w-full  bg-[#aba1a180] rounded-full" />
       </div>
 
-      <div className='px-20 pt-10 '>
+      <div className='lg:px-20 md:px-10 px-1 pt-10 '>
         <h1 className='font-bold text-2xl pb-4'>Contact me</h1>
 
-        <div className='grid lg:grid-cols-2  lg:gap-4 md:gap-10 gap-4'>
+        <div className='flex flex-col md:flex-row items-start justify-between lg:gap-8 md:gap-10 gap-8'>
 
-          <div>
-            <h2 className='font-bold text-lg pb-2'>Get in touch</h2>
+          <div className=''>
+            <h2 className='font-bold text-lg pb-3'>Get in touch</h2>
 
             <div className='flex flex-col gap-3'>
 
@@ -28,9 +28,9 @@ const Contact = () => {
                 <div className='p-1.5 peer bg-[#272626] text-[#cacccf] hover:text-[#dbdcde] rounded-full hover:scale-105 transition duration-200 hover:-translate-y-0.5 hover:bg-[#3a3a3a]'>
                   <MdOutlineMailOutline size={26} />
                 </div>
-                <div className='text-[#abaeb2] text-lg peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
+                <div className='text-[#abaeb2] text-sm md:text-base peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
                   <p>Email</p>
-                  <p className='text-[#ededed]'>Pallab861774@gmail.com</p>
+                  <p className='text-[#ededed] break-all block'>Pallab861774@gmail.com</p>
                 </div>
               </div>
 
@@ -38,9 +38,9 @@ const Contact = () => {
                 <div className='p-1.5 peer bg-[#272626] text-[#cacccf] hover:text-[#dbdcde] rounded-full hover:scale-105 transition duration-200 hover:-translate-y-0.5 hover:bg-[#3a3a3a]'>
                   <FaPhone size={26} />
                 </div>
-                <div className='text-[#abaeb2] text-lg peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
+                <div className='text-[#abaeb2] text-sm md:text-base peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
                   <p>Phone no.</p>
-                  <p className='text-[#ededed]'>0123456789</p>
+                  <p className='text-[#ededed] break-all block'>0123456789</p>
                 </div>
               </div>
 
@@ -48,9 +48,9 @@ const Contact = () => {
                 <div className='p-1.5 peer bg-[#272626] text-[#cacccf] hover:text-[#dbdcde] rounded-full hover:scale-105 transition duration-200 hover:-translate-y-0.5 hover:bg-[#3a3a3a]'>
                   <IoLocationOutline size={26} />
                 </div>
-                <div className='text-[#abaeb2] text-lg peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
+                <div className='text-[#abaeb2] text-sm md:text-base peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
                   <p>Location</p>
-                  <p className='text-[#ededed]'>Singur , Hooghly , India</p>
+                  <p className='text-[#ededed] break-all block'>Singur , Hooghly , India</p>
                 </div>
               </div>
 
@@ -58,9 +58,9 @@ const Contact = () => {
                 <div className='p-1.5 peer bg-[#272626] text-[#cacccf] hover:text-[#dbdcde] rounded-full hover:scale-105 transition duration-200 hover:-translate-y-0.5 hover:bg-[#3a3a3a]'>
                   <CiLinkedin size={26} />
                 </div>
-                <div className='text-[#abaeb2] text-lg peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
+                <div className='text-[#abaeb2] text-sm md:text-base peer-hover:text-[#dbdcde] transition duration-200 peer-hover:scale-y-100  peer-hover:-translate-y-0.5'>
                   <p>LinkedIn</p>
-                  <Link to={"https://www.linkedin.com/in/pallab-bag-35115a2b1/"} className='text-[#ededed]'>https://www.linkedin.com/in/pallab-bag-35115a2b1/</Link>
+                  <Link to={"https://www.linkedin.com/in/pallab-bag-35115a2b1/"} className='text-[#ededed] break-all block'>https://www.linkedin.com/in/pallab-bag-35115a2b1/</Link>
                 </div>
               </div>
 
@@ -68,38 +68,38 @@ const Contact = () => {
 
           </div>
 
-          <div>
+          <div className='lg:pr-24'>
 
-            <h2 className='font-bold text-lg pb-2'>Send me a message</h2>
+            <h2 className='font-bold text-lg pb-3'>Send me a message</h2>
 
             <div className='bg-primary-dark rounded-xl p-6'>
-              <div className='font-bold text-lg pb-6 flex gap-4 justify-between'>
+              <div className=' text-sm pb-3 flex flex-row flex-wrap justify-between items-center gap-3'>
 
                 <div className='group'>
 
-                  <p className='group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1'>Name</p>
-                  <input type="text" className='bg-[#444444f2] rounded outline-none' />
+                  <p className='group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1 font-semibold '>Name</p>
+                  <input type="text" className='bg-[#444444f2] rounded outline-none w-full p-2' />
 
                 </div>
 
                 <div className='group'>
 
-                  <p className='group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1'>Email</p>
-                  <input type="email" className='bg-[#444444f2] rounded outline-none' />
+                  <p className='group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1 font-semibold'>Email</p>
+                  <input type="email" className='bg-[#444444f2] rounded outline-none w-full p-2' />
 
                 </div>
 
               </div>
 
-              <div className='group font-bold text-lg'>
+              <div className='group  text-sm'>
 
-                <p className='group-hover:scale-y-105 transition-all duration-500 group-hover:-translate-y-1'>Message</p>
-                <input type="text" className='bg-[#444444f2] rounded outline-none h-16 w-full' />
+                <p className='group-hover:scale-y-105 font-semibold transition-all duration-500 group-hover:-translate-y-1'>Message</p>
+                <textarea name="" id="" className='bg-[#444444f2] rounded outline-none h-16 w-full p-2'></textarea>
 
               </div>
 
-              <div className='pt-4 font-bold'>
-                <input type="submit" className='bg-[#444444f2] rounded outline-none w-full h-10' />
+              <div className='pt-4 font-semibold '>
+                <input type="submit" className='bg-[#444444f2] hover:bg-[#393939f2] rounded outline-none w-full h-8' />
               </div>
 
             </div>
