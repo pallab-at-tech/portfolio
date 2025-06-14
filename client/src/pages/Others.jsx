ce22ed263090fbf82ab41cb53a5bada01eb4cc4b
pallab-at-tech <pallab861774@gmail.com>
2025-06-14 11:25:10 +0530

    till now responsive

--- client/src/pages/Others.jsx
+++ client/src/pages/Others.jsx
@@ -8,43 +8,77 @@ const Others = () => {
 
             <h1 className='px-16 font-bold text-2xl md:pt-16 mb-6'>Achievement</h1>
 
-            <div className='md:px-16 px-8 flex  justify-center flex-wrap gap-6'>
+            <div className='md:px-16 px-8 flex  justify-center flex-wrap gap-y-16 pb-7'>
 
-                <div className='md:min-w-[600px] min-w-[300px] max-w-[300px] min-h-[350px] max-h-[350px] md:min-h-[300px] md:max-w-[600px] md:max-h-[300px] bg-[#514f4f] rounded mb-6 flex md:flex-row  flex-col-reverse gap-4 px-6 md:py-0 py-4'>
+                <div className='lg:min-w-[800px] lg:max-w-[800px] lg:min-h-[300px]  lg:max-h-[300px] md:min-w-[530px] md:max-w-[530px] md:min-h-[610px]  md:max-h-[610px] min-w-[280px] max-w-[280px] min-h-[350px]  max-h-[350px] bg-[#514f4f] rounded grid lg:grid-cols-2  items-center justify-start lg:px-0 lg:py-0 px-6 py-2  shadow-[inset_0_0_14px_4px_rgba(147,51,234,0.45),0_0_4px_1px_rgba(147,51,234,0.45)]'>
 
-                    <div className='md:min-w-[60%] md:max-w-[60%] min-w-full max-w-full flex items-center'>
-                        <img src={c1} alt="" className='' />
+                    <div className='items-center flex-col pb-2 lg:hidden flex'>
+                        <p className='py-1 font-semibold'>My thought</p>
+                        <p className='md:text-sm text-xs md:line-clamp-[7] line-clamp-5 pr-7 pl-4 md:leading-relaxed leading-tight  lg:min-h-[240px] lg:max-h-[240px] lg:min-w-[370px] lg:max-w-[370px] md:min-h-[176px] md:max-h-[176px] md:min-w-[480px] md:max-w-[480px]  min-h-[85px] max-h-[85px] min-w-[240px] max-w-[240px] break-words overflow-hidden  p-2 rounded text-gray-800 bg-[#c5c2bb]'>
+                            <p className='font-semibold'>🚀 Attended "AI For All" Workshop | March 24–28, 2025 🤖</p>
+
+                            <p className='min-h-1'> </p>
+
+                            <p>I recently had the incredible opportunity to be part of the "AI For All" workshop—a week-long journey into the world of Artificial Intelligence! 🧠✨ From understanding the basics of AI and machine learning to exploring real-world applications, this experience helped me gain valuable insights into how AI is transforming every field—from healthcare and education to business and creativity.sjhjas asjguguas asguygasd ytftyftyfaxs ghfasytfyasx tyfasxtyfytasx tyfsaytfytxas tyfyastfytas tyfytras TYFYTr6 sxaytfytraxs hytfzxytftraxs fctsxrfsax ytsftrxsx  💡</p>
+                        </p>
                     </div>
 
-                    <div className='text-sm flex items-center '>
-                        <p className='md:leading-relaxed leading-tight md:max-h-[232px] max-h-[125px] min-h-[125px]  break-words overflow-y-auto scrollbar-custom p-2 rounded text-gray-800 bg-[#c1b185]'>
-                            I had the amazing opportunity to attend my very first college webinar on Integrated Personality Development! 🧠✨
+                    
 
-                            This session helped me understand how developing a strong personality is not just about communication or confidence—it's about building the right mindset, values, and life skills that shape who we are, both personally and professionally. 💼💬
+                    <div className=' justify-center  flex flex-col items-center pr-2 pb-2 hover:scale-105 transition hover:-translate-y-0.5 duration-200'>
+                        <p className='py-1 font-semibold'>Certificate</p>
+                        <img src={c2} alt="" className='lg:w-[350px] md:w-[473px] w-[240px] ' />
+                    </div>
+
+                    
+
+                    <div className='items-center flex-col pb-2 lg:flex hidden'>
+                        <p className='py-1 font-semibold'>My thought</p>
+                        <p className='text-sm line-clamp-[10] pr-7 pl-4 leading-relaxed lg:min-h-[240px] lg:max-h-[240px] lg:min-w-[370px] lg:max-w-[370px]  break-words overflow-hidden  p-2 rounded text-gray-800 bg-[#c5c2bb]'>
+                            <p className='font-semibold'>🚀 Attended "AI For All" Workshop | March 24–28, 2025 🤖</p>
+
+                            <p className='min-h-1'> </p>
+
+                            <p>I recently had the incredible opportunity to be part of the "AI For All" workshop—a week-long journey into the world of Artificial Intelligence! 🧠✨ From understanding the basics of AI and machine learning to exploring real-world applications, this experience helped me gain valuable insights into how AI is transforming every field—from healthcare and education to business and creativity. 💡</p>
                         </p>
                     </div>
 
                 </div>
 
-                <div className='md:min-w-[600px] min-w-[300px] max-w-[300px] min-h-[350px] max-h-[350px] md:min-h-[300px] md:max-w-[600px] md:max-h-[300px] bg-[#514f4f] rounded mb-6 flex md:flex-row  flex-col-reverse gap-4 px-6 md:py-0 py-4'>
+                <div className='lg:min-w-[800px] lg:max-w-[800px] lg:min-h-[300px]  lg:max-h-[300px] md:min-w-[530px] md:max-w-[530px] md:min-h-[610px]  md:max-h-[610px] min-w-[280px] max-w-[280px] min-h-[350px]  max-h-[350px] bg-[#514f4f] rounded grid lg:grid-cols-2  items-center justify-start lg:px-0 lg:py-0 px-6 py-2 shadow-[inset_0_0_14px_4px_rgba(239,68,68,0.5),0_0_4px_1px_rgba(239,68,68,0.5)]'>
 
-                    <div className='md:min-w-[60%] md:max-w-[60%] min-w-full max-w-full flex items-center'>
-                        <img src={c2} alt="" className='' />
+                    <div className='items-center flex-col pb-2 lg:hidden flex'>
+                        <p className='py-1 font-semibold'>My thought</p>
+                        <p className='md:text-sm text-xs md:line-clamp-[7] line-clamp-5 pr-7 pl-4 md:leading-relaxed leading-tight  lg:min-h-[240px] lg:max-h-[240px] lg:min-w-[370px] lg:max-w-[370px] md:min-h-[176px] md:max-h-[176px] md:min-w-[480px] md:max-w-[480px]  min-h-[85px] max-h-[85px] min-w-[240px] max-w-[240px] break-words overflow-hidden  p-2 rounded text-gray-800 bg-[#c5c2bb]'>
+                            <p className='font-semibold'>I had the amazing opportunity to attend my very first college webinar on Integrated Personality Development! 🧠✨</p>
+
+                            <p className='min-h-1'> </p>
+
+                            <p>This session helped me understand how developing a strong personality is not just about communication or confidence—it's about building the right mindset, values, and life skills that shape who we are, both personally and professionally.</p>
+                        </p>
                     </div>
 
-                    <div className='text-sm flex items-center '>
-                        <p className='md:leading-relaxed leading-tight md:max-h-[232px] max-h-[125px] min-h-[125px]  break-words overflow-y-auto scrollbar-custom p-2 rounded text-gray-800 bg-[#c1b185]'>
-                             🚀 Attended "AI For All" Workshop | March 24–28, 2025 🤖
+                    
 
-                            I recently had the incredible opportunity to be part of the "AI For All" workshop—a week-long journey into the world of Artificial Intelligence! 🧠✨
+                    <div className=' justify-center  flex flex-col items-center pr-2 pb-2  hover:scale-105 transition hover:-translate-y-0.5 duration-200'>
+                        <p className='py-1 font-semibold'>Certificate</p>
+                        <img src={c1} alt="" className='lg:w-[350px] md:w-[473px] w-[240px]' />
+                    </div>
 
-                            From understanding the basics of AI and machine learning to exploring real-world applications, this experience helped me gain valuable insights into how AI is transforming every field—from healthcare and education to business and creativity. 💡
+                    
+
+                    <div className='items-center flex-col pb-2 lg:flex hidden'>
+                        <p className='py-1 font-semibold'>My thought</p>
+                        <p className='text-sm line-clamp-[10] pr-7 pl-4 leading-relaxed lg:min-h-[240px] lg:max-h-[240px] lg:min-w-[370px] lg:max-w-[370px]  break-words overflow-hidden  p-2 rounded text-gray-800 bg-[#c5c2bb]'>
+                            <p className='font-semibold'>I had the amazing opportunity to attend my very first college webinar on Integrated Personality Development! 🧠✨</p>
+
+                            <p className='min-h-1'> </p>
+
+                            <p>This session helped me understand how developing a strong personality is not just about communication or confidence—it's about building the right mindset, values, and life skills that shape who we are, both personally and professionally.</p>
                         </p>
                     </div>
 
                 </div>
-
- 
 
             </div>
 
