4f1001674b154a009f85995d3305418423f312aa
pallab-at-tech <pallab861774@gmail.com>
2025-07-02 20:21:08 +0530

    ui designed

--- client/src/pages/Others.jsx
+++ client/src/pages/Others.jsx
@@ -9,6 +9,7 @@ import { OthersDetails } from '../store/otherSlice';
 import AxiosTostError from '../utils/AxiosToastError';
 import { OthersDetailspro } from '../store/OtherScrollData';
 import Footer from '../components/Footer';
+import { useGlobalContext } from '../provider/GlobalProvider';
 
 const Others = () => {
 
@@ -24,9 +25,6 @@ const Others = () => {
         totalNoOfPage: otherData?.totalNoOfPage
     })
     const [currentIndex, setCurrentIndex] = useState(0);
-
-    console.log("allOf", allOf)
-
 
     const fetchOtherData = async () => {
 
@@ -115,25 +113,26 @@ const Others = () => {
 
     }, [pagination.totalNoOfPage])
 
+    const { darkMode, setDarkMode } = useGlobalContext();
 
 
-
+// #000000 black
 
     return (
-        <section className='min-h-[100vh] bg-primary-dark text-primary-text extra-font-style md:px-14 pt-[72px] lg:px-6 px-2 relative'>
+        <section className={`min-h-[100vh] ${darkMode ? "bg-primary-dark" : "card-bg-color-light"} text-primary-text lato-regular md:px-14 pt-[72px] lg:px-6 px-2 relative`}>
 
-            <h1 className='px-16 font-bold text-2xl md:pt-2  flex items-center flex-col'>Achievement</h1>
+            <h1 className={`px-16 font-bold text-2xl md:pt-2  flex items-center flex-col ${!darkMode && "text-[#000000] font-bold"}`}>Achievement</h1>
 
 
 
             <div className='w-full'>
 
-                <div className='lg:ml-28  lg:absolute top-[72px] '>
+                <div className={`lg:ml-28  lg:absolute top-[72px] ${!darkMode && "text-[#000000]"}`}>
                     <Link to={"/"} className='w-fit'>
-                        <div className='ml-4'>
+                        <div className={`ml-4 `}>
                             <FaHome size={28} />
                         </div>
-                        <p className='ml-2'>Home</p>
+                        <p className='ml-2 font-bold'>Home</p>
                     </Link>
                 </div>
 
@@ -141,14 +140,14 @@ const Others = () => {
 
                 {/* here recent one */}
                 <div className='mx-auto container lg:max-w-[1280px] max-w-[995px] mt-16 md:px-0 px-4'>
-                    <p className='font-semibold text-xl my-1 pl-4'>Most recently..</p>
+                    <p className={`font-semibold text-xl my-1 pl-4 ${!darkMode && "text-[#a90707]"}`}>Most recently..</p>
 
                     <div className='lg:max-w-[1280px] max-w-[995px] gap-6 flex  overflow-x-auto  scrollbar-hidden no-interaction'>
 
                         {
                             allOf.all_certificate.map((v, i) => {
                                 return (
-                                    <Link to={`/Others/${v._id}`} key={`${v._id} - ${i}`} className='min-w-full max-w-full  shrink-0 bg-[#2b2b2b] p-3 rounded shadow-md my-4 transition-transform duration-700 ease-in-out'
+                                    <Link to={`/Others/${v._id}`} key={`${v._id} - ${i}`} className={`min-w-full max-w-full  shrink-0 ${darkMode ? "bg-[#2b2b2b] shadow-[inset_0_0_12px_rgba(255,245,200,0.15),inset_0_0_6px_rgba(255,255,255,0.05)]" : "card-other-section-light"} p-3 rounded  my-4 transition-transform duration-700 ease-in-out`}
 
                                         style={{
                                             transform: `translateX(-${currentIndex * 100}%)`,
@@ -168,7 +167,7 @@ const Others = () => {
 
                                             <div className=''>
                                                 <h1 className='font-semibold text-lg pb-4 lg:line-clamp-none md:line-clamp-[11] text-amber-300'>{v.tittle}</h1>
-                                                <pre className="text-sm text-white whitespace-pre-wrap lg:block hidden">{v.describe}</pre>
+                                                <pre className="text-sm lato-regular text-white whitespace-pre-wrap lg:block hidden">{v.describe}</pre>
                                             </div>
 
                                         </div>
@@ -187,7 +186,7 @@ const Others = () => {
 
                 <div className='mx-auto container lg:max-w-[1280px] max-w-[995px] py-14'>
 
-                    <p className='font-semibold text-xl mt-4 pl-4'>Others..</p>
+                    <p className={`font-semibold text-xl mt-4 pl-4 ${!darkMode && "text-[#a90707]"}`}>Others..</p>
 
                     <div className='lg:max-w-[1280px]  max-w-[995px] overflow-x-auto flex  gap-6 px-6 py-4 scroll-smooth scrollbar-custom' ref={scrollRef}>
 
@@ -195,7 +194,7 @@ const Others = () => {
                         {otherData?.data?.map((val, idx) => (
                             <Link to={`/Others/${val._id}`}
                                 key={`${val._id} + ${idx}`}
-                                className='min-w-[300px] max-w-[300px] shrink-0 bg-[#2b2b2b] p-3 rounded shadow-md'
+                                className={`min-w-[300px] max-w-[300px] shrink-0 ${darkMode ? "bg-[#2b2b2b] shadow-md hover:shadow-[inset_0_0_16px_rgba(255,230,120,0.18),inset_0_0_8px_rgba(255,230,120,0.1)]" : "card-other-small-section-light"}  p-3 rounded   transition-shadow duration-400`}
                             >
                                 <h1 className='line-clamp-2 font-semibold'>{val.tittle}</h1>
                                 <Link to={`/Others/${val._id}`} className='text-sm text-blue-400 hover:underline'>know more</Link>
@@ -218,9 +217,9 @@ const Others = () => {
 
             </div>
 
-            <div className=' pb-4'>
+            {/* <div className=' pb-4'>
                 <Footer />
-            </div>
+            </div> */}
 
 
         </section>
