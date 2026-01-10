ac4f352581111b245cca1d0d696048f4fbd99779
pallab-at-tech <pallab861774@gmail.com>
2026-01-11 00:53:22 +0530

    home page improved

--- client/src/features/AdminPermission.jsx
+++ client/src/features/AdminPermission.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
-const AdminPermission = ({children}) => {
+const AdminPermission = ({ children }) => {
 
     const user = useSelector((state) => state?.user)
 
 
     return (
-        <>
-
-        {
-            user?.admin_verify ? children : <p className='text-red-700 bg-red-200 p-4'>Do not have permission</p>
-        }
-
-        </>
+        <div className='h-[calc(100vh-72px)] flex items-center justify-center mx-4'>
+            {
+                user?.admin_verify ? children : <p className='text-red-700 bg-red-200 border border-red-400 rounded-md p-4'>Do not have permission</p>
+            }
+        </div>
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -40,18 +40,19 @@ const Home = () => {
     <div className='h-screen overflow-y-auto hide-scrollbar' id='fullScreenId' >
 
       {/* Home components */}
-      <div className={`h-screen flex flex-col overflow-hidden ${darkMode ? "background-image" : "background-image-light"}  pt-[72px]`} id='realHomeId'>
+      <div className={`h-screen flex flex-col overflow-hidden ${darkMode ? "background-image" : "background-image-light"} pt-[72px]`} id='realHomeId'>
 
         <div id='HomeID' className='relative flex-1'>
 
-          <div className='absolute overflow-hidden -top-14 right-1 rotate-185 scale-120 object-scale-down select-none lg:block hidden'>
+          {/* Animated leave png */}
+          <div className='absolute overflow-hidden -top-14 sm:-top-[140px] right-1 sm:right-7 rotate-[185deg] sm:rotate-[175deg] scale-120 sm:scale-[140%]  object-scale-down select-none sm:block hidden'>
             <img src={b} alt="" className="h-[500px] object-cover z-0 left-right-animation overflow-hidden" />
           </div>
 
-          <div className='grid lg:grid-cols-[2fr_3fr] lg:mx-10 md:mx-[140px] pt-2  p-10 md:p-0 lg:pt-14 md:pt-14'>
+          {/* for window */}
+          <div className='lg:grid hidden lg:grid-cols-[2fr_3fr] mx-10 p-0 pt-14'>
 
-            {/* for desktop */}
-            <div className='items-center justify-center p-3 mt-14 lg:pl-16 lg:flex hidden'>
+            <div className='items-center justify-center p-3 mt-14 pl-16 flex'>
 
               <div className={`${darkMode ? "outer-box" : "outer-box-light"}`}>
 
@@ -63,24 +64,6 @@ const Home = () => {
 
               </div>
 
-            </div>
-
-            {/* for tablet */}
-            <div className='items-center justify-center p-3 mt-6 lg:hidden md:flex hidden'>
-              <div className={`${darkMode ? "outer-box" : "outer-box-light-md"}`}>
-                <div className={`${darkMode ? "card" : "card-light-md"}`}>
-
-                  <img src={pic} alt="" className='w-[220px] h-[234px] object-top m-4 object-cover rounded-3xl' />
-
-                </div>
-              </div>
-            </div>
-
-            {/* for mobile */}
-            <div className='items-center justify-center flex md:hidden'>
-              <div className='relative'>
-                <img src={pic} alt="" className='w-[180px] h-[180px] object-top object-cover rounded-full' />
-              </div>
             </div>
 
             <div className='flex'>
@@ -114,12 +97,176 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
 
+          {/* for mobile and tablet */}
+          <div className='flex lg:hidden items-center justify-center w-full h-screen'>
+            <div className='grid grid-rows-[120px_1fr_1fr] sm:grid-rows-[160px_1fr_1fr] px-10 py-5 sm:px-[100px] sm:py-10'>
+
+              {/* Tittle and text section */}
+              <div className={`leading-[1.01] text-4xl sm:text-[46px] ${darkMode ? "text-primary-text" : "text-primary-light-text"}`}>
+
+                <div>
+                  <span className={`text-[45px] sm:text-[60px] ${darkMode ? "first-dark-mode" : "first-light-mode"} font-semibold`}>H</span>
+                  <span>ello {" ,"}</span>
+                </div>
+
+                <div>
+                  <span className='text-[45px] sm:text-[60px]'>I</span> 'm {" "}
+                  <span
+                    className={`${darkMode ? "text-terniary-dark" : "text-[#a80000]"} font-semibold`} style={darkMode ? { textShadow: 'rgb(187 80 8 / 50%) 1px 1px 8px' } : { textShadow: 'rgb(187 0 0 / 50%) 2px 0px 6px' }}
+                  >
+                    Pallab Bag
+                  </span>
+                </div>
+
+                <div className='text-[17px] flex items-center gap-2 py-2'>
+                  <div className={`h-[6px] w-[6px] ${darkMode ? "bg-sky-500" : "bg-amber-800 "} rounded-full`}></div>
+                  <span className={`${darkMode ? "text-[#bdcbebeb]" : "text-[#481e03d8]"}`}>MERN STACK DEVELOPER</span>
+                </div>
+              </div>
+
+              {/* profile and social media section */}
+              <div className='grid grid-cols-[1fr_80px] pt-5 pb-5 '>
+
+                <div className='relative w-fit h-fit'>
+                  {/* Image layer */}
+                  <div className={`relative rounded-2xl p-1.5 border ${darkMode ? "border-blue-800" : "border-[#92220690]"} overflow-hidden`}>
+                    {/* Neon spotlight */}
+                    <div className={`absolute inset-0 rounded-2xl ${darkMode ? "neon-spotlight-dark" : "neon-spotlight-light"}`}></div>
+
+                    {/* Image */}
+                    <img
+                      src={pic}
+                      alt=""
+                      className="relative z-10 h-[28vh] sm:h-[29vh] w-full rounded-xl object-contain"
+                    />
+                  </div>
+
+                  {/* social media section */}
+                  <div className="absolute -right-[65px] sm:-right-[230px] bottom-2 space-y-3 sm:space-y-4">
+
+                    <div className='flex items-center gap-4'>
+                      <Link
+                        to={allOf.linkedin_link}
+                        className={`
+                            group
+                            flex items-center justify-center
+                            w-11 h-11
+                            rounded-xl
+                            border border-amber-700
+                            transition-all duration-300
+                            ${darkMode ? "bg-[#913800] text-[#59b806] shadow-[0_0_5px_0_rgb(89,184,6,0.4)] hover:shadow-[0_0_18px_rgba(89,184,6,0.6)]" : "bg-[#fee7c0] text-black hover:shadow-lg"}
+                        `}
+                      >
+                        <FaLinkedin
+                          size={22}
+                          className="transition-transform duration-300 group-hover:scale-110"
+                        />
+                      </Link>
+
+                      <div className={`items-center gap-2 hidden sm:flex ${darkMode ? "text-white" : "text-[#381500]"} `}>
+                        <div className={`h-[6px] w-[6px] rounded-full ${darkMode ? "bg-green-500" : "bg-red-900"}`}></div>
+
+                        <Link to={allOf.linkedin_link} >
+                          My linkedIn.Com
+                        </Link>
+                      </div>
+                    </div>
+
+                    <div className='flex items-center gap-4'>
+                      <Link
+                        to={allOf.github_link}
+                        className={`
+                        group
+                        flex items-center justify-center
+                        w-11 h-11
+                        rounded-xl
+                        border border-amber-700
+                        transition-all duration-300
+                        ${darkMode ? "bg-[#913800] text-[#59b806] shadow-[0_0_5px_0_rgb(89,184,6,0.4)] hover:shadow-[0_0_18px_rgba(89,184,6,0.6)]" : "bg-[#fee7c0] text-black hover:shadow-lg"}
+                      `}
+                      >
+                        <FaGithub
+                          size={22}
+                          className="transition-transform duration-300 group-hover:scale-110"
+                        />
+                      </Link>
+
+                      <div className={`items-center gap-2 hidden sm:flex ${darkMode ? "text-white" : "text-[#381500]"} `}>
+                        <div className={`h-[6px] w-[6px] rounded-full ${darkMode ? "bg-green-500" : "bg-red-900"}`}></div>
+
+                        <Link to={allOf.github_link} className=''>
+                          My Github.Com
+                        </Link>
+                      </div>
+                    </div>
+
+                    <div className='flex items-center gap-4'>
+
+                      <Link
+                        to={`mailto:${allOf.email}`}
+                        className={`
+                          group
+                          flex items-center justify-center
+                          w-11 h-11
+                          rounded-xl
+                          border border-amber-700
+                          transition-all duration-300
+                          ${darkMode ? "bg-[#913800] text-[#59b806] shadow-[0_0_5px_0_rgb(89,184,6,0.4)] hover:shadow-[0_0_18px_rgba(89,184,6,0.6)]" : "bg-[#fee7c0] text-black hover:shadow-lg"}
+                        `}
+                      >
+                        <SiGmail
+                          size={20}
+                          className="transition-transform duration-300 group-hover:scale-110"
+                        />
+                      </Link>
+
+                      <div className={`items-center gap-2 hidden sm:flex ${darkMode ? "text-white" : "text-[#381500]"} `}>
+                        <div className={`h-[6px] w-[6px] rounded-full ${darkMode ? "bg-green-500" : "bg-red-900"}`}></div>
+
+                        <Link to={`mailto:${allOf.email}`} >
+                          My Gmail.Com
+                        </Link>
+                      </div>
+
+                    </div>
+
+                  </div>
+                </div>
+
+              </div>
+
+              {/* little bit about and download cv */}
+              <div className='flex flex-col sm:gap-10 gap-6 mt-0 sm:mt-4'>
+
+                <div className={`text-sm sm:text-lg mt-1 ${darkMode ? "text-[#747573]" : "text-[#332301]"} pl-1 pr-5`}>
+                  <p className=''>
+                    A goal-oriented software developer with experience in building applications using modern technologies like <span className={`${darkMode ? "text-[#59b806]" : "text-[#a80202]"} font-semibold`}>React, MongoDB, Express.js, Node.js, and more</span>. I am seeking to leverage my technical skills to deliver exceptional user experiences.
+                  </p>
+                </div>
+
+                <div
+                  onClick={() => downloadFileAtURL(resume)}
+                  className={`flex items-center justify-center shadow-md gap-0.5 rounded-md px-4 sm:px-5 py-[8px] sm:py-[10px] w-fit ${darkMode ? "bg-blue-700 text-white" : "bg-[#fee7c0] text-amber-900"}`}
+                >
+                  <IoMdDownload size={22} className='hidden sm:block' />
+                  <IoMdDownload size={20} className='block sm:hidden' />
+                  <p className='text-base sm:text-lg'>Download Resume</p>
+                </div>
+
+                <div className='flex items-center justify-center sm:my-3 text-[#5e5b5b] animate-float'>
+                  <LuChevronsDown size={32} className='hidden sm:block'/>
+                  <LuChevronsDown size={22} className='block sm:hidden'/>
+                </div>
+
+              </div>
+            </div>
           </div>
 
         </div>
 
-        <div className='flex items-center justify-center my-3 text-[#5e5b5b] animate-float'>
+        <div className='lg:flex hidden items-center justify-center my-3 text-[#5e5b5b] animate-float'>
           <LuChevronsDown size={26} />
         </div>
 
